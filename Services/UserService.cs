6695b993b9e0214fa3517b9e3b4c48fdfb46dcde
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-12-09 21:12:11 +0100

    Add UserService

---
+++ Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using PCServ.Context;
+
+namespace PCServ.Services
+{
+    public class UserService
+    {
+        public UserService(AppDbContext appDbContext)
+        {
+        }
+    }
+}

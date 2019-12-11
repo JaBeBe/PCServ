6044ebdd31273266c31f3a40f1f0eedbe1a42855
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-12-11 20:22:47 +0100

    Add GetUser method in UserService

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using PCServ.Context;
+using PCServ.Models.User;
 
 namespace PCServ.Services
 {
     public class UserService
     {
+        private AppDbContext _appDbContext;
         public UserService(AppDbContext appDbContext)
         {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<User> GetUser(string login)
+        {
+            return await _appDbContext.Users.Where(u => u.Login == login).FirstOrDefaultAsync();
         }
     }
 }

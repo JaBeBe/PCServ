5013ee830ea50e4b8e310686a64d523404afb73d
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2019-12-11 20:43:36 +0100

    Add PasswordHelper class with methods Hash and Verifiy

---
+++ Helpers/PasswordHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PCServ.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static string Hash(string password, string salt = "")
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password+salt);
+            byte[] hashBytes;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                hashBytes = sha256.ComputeHash(passwordBytes);
+            }
+
+            return Encoding.Default.GetString(hashBytes);
+        }
+
+        public static bool Verify(string password, string hash, string salt="")
+        {
+            string passwordHash = PasswordHelper.Hash(password, salt);
+            return hash.Equals(passwordHash);
+        }
+    }
+}

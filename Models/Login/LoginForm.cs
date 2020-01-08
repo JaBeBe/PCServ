3963f2047eb72f9b32f787058e27818888ddb63a
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2020-01-08 21:43:47 +0100

    Add LoginForm class

---
+++ Models/Login/LoginForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PCServ.Models.Login
+{
+    public class LoginForm
+    {
+        public LoginForm()
+        {
+        }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}

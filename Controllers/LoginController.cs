d2cd2e861a237f84fa79bb4627781f348babd53a
Krzysztof Pytel <37655632+krzysztof97@users.noreply.github.com>
2020-01-08 23:22:04 +0100

    Add Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PCServ.Models.Login;
+using PCServ.Services;
+
+namespace PCServ.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        public IUserService _userService;
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Authenticate([FromBody]LoginForm loginForm)
+        {
+            var user = _userService.LoginUser(loginForm);
+
+            if (user == null)
+                return BadRequest(new { message = "Bad login credentials" });
+
+            return Ok(user);
+        }
+    }
+}

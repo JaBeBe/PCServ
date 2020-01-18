aef27ddcf1d2649c3c832a6c0c326a6b8670a6b3
JaBeBe <jacek.a.bebak@gmail.com>
2020-01-18 10:45:31 +0100

    Add models: Product, ServiceRequest, RequestHistory

---
+++ Models/ServRequestRepo/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCServ.Models.ServRequestRepo
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SerialNo { get; set; }
+
+    }
+}
---
+++ Models/ServRequestRepo/RequestHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCServ.Models.ServRequestRepo
+{
+    public class RequestHistory
+    {
+        public int id { get; set; }
+        public ServiceRequest Request { get; set; }
+        public User.User ServiceMan { get; set; }
+        public User.User Client { get; set; }
+        public string Description { get; set; }
+        public DateTime CtreateAt { get; set; }
+    }
+}

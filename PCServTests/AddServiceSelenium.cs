f66c2e71751a5d509485e8e4c28b818fb03b5e71
JaBeBe <jacek.a.bebak@gmail.com>
2020-02-01 13:36:11 +0100

    add tests (UserController and selenium Add ServiceReq)

---
+++ PCServTests/AddServiceSelenium.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace PCServTests
+{
+    [TestFixture]
+    class AddServiceSelenium
+    {
+        IWebDriver _driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.AcceptInsecureCertificates = true;
+            _driver = new FirefoxDriver(options);
+        }
+        [Test]
+        public void RedirectToDashboardAfterAuthentication()
+        {
+            // go to out website
+            _driver.Navigate().GoToUrl("https://localhost:5001");
+
+            // click login url
+            var loginLink = _driver.FindElement(By.CssSelector("[href='/new-request']"));
+            loginLink.Click();
+
+            // wait for redirect
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(3))
+                .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.TagName("app-add-service-request"))));
+
+            var titleField = _driver.FindElement(By.Name("title"));
+            titleField.SendKeys("jakis");
+
+            var descriptionField = _driver.FindElement(By.Name("description"));
+            descriptionField.SendKeys("password");
+
+
+            var productField = _driver.FindElement(By.Name("product"));
+
+            // get submit button and send form
+            var submitButton = _driver.FindElement(By.CssSelector("[type='submit']"));
+            submitButton.Click();
+
+            // wait for reload
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(3))
+                .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.TagName("app-dashboard"))));
+
+            StringAssert.StartsWith("https://localhost:5001/dashboard", _driver.Url);
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _driver.Close();
+        }
+    }
+}
